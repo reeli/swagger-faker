14e24cb157e4a5e734ac257aa510382da9a3dd83
reeli <reeli.null@gmail.com>
2019-07-27 15:16:42 +0800

    generate response by operation id

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as fs from "fs";
 import { Spec } from "swagger-schema-official";
 import { Traverse } from "./traverse";
 import { getAllFaker, getFaker } from "./faker";
+import { forEach, get, pick } from "lodash";
+import { pickRefKey } from "./utils";
 
 export function printExamples(operationId?: string) {
   const schemaStr = fs.readFileSync("./examples/swagger.json", "utf8");
@@ -23,9 +25,29 @@ export function printExamples(operationId?: string) {
     fs.writeFileSync("./.output/test.json", JSON.stringify(data, null, 2), "utf-8");
 
     if (operationId) {
+      console.log(getResponseByOperationId(schema, operationId));
       fs.writeFileSync(`./.output/${operationId}.json`, JSON.stringify(getFaker(data[operationId]), null, 2), "utf-8");
     } else {
       fs.writeFileSync(`./.output/mock-data.json`, JSON.stringify(getAllFaker(data), null, 2), "utf-8");
     }
   }
 }
+
+const getResponseByOperationId = (spec: Spec, operationId: string) => {
+  let res = null;
+  forEach(spec.paths, (path, pathName) => {
+    const operations = pick(path, ["get", "post", "put", "delete", "patch", "options", "head"]);
+    Object.keys(operations).map((method) => {
+      // TODO: remove any
+      if ((path as any)[method].operationId === operationId) {
+        const p = pathName.replace(/\{/g, ":").replace(/\}/g, "");
+        res = {
+          path: spec.basePath ? `${spec.basePath}${p}` : p,
+          method,
+          responses: pickRefKey(get((path as any)[method], "responses.200.schema.$ref")),
+        };
+      }
+    });
+  });
+  return res;
+};
