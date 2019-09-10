9dd2af74b8eccca090096a1a6b0b2690a67d6d6a
reeli <reeli.null@gmail.com>
2019-09-10 15:10:25 +0800

    handle array items when traverse

--- src/__tests__/traverse.test.ts
+++ src/__tests__/traverse.test.ts
@@ -1,3 +1,9 @@
+import { Traverse } from "../traverse";
+import { expectedValue, traverseMocks } from "./mock-data/traverseMocks";
+
 describe("traverse", () => {
-  it("should handle ref correctly", () => {});
+  it("should replace ref", () => {
+    const data = Traverse.of(traverseMocks).traverse();
+    expect(data).toEqual(expectedValue);
+  });
 });
