12cd60532e2cce2400dfdbb043255c38f1766929
reeli <reeli.null@gmail.com>
2019-09-19 15:20:50 +0800

    remove useless code

--- src/command.ts
+++
@@ -1,6 +0,0 @@
-import program from "commander";
-import { printExamples } from "./index";
-
-program.option("-o, --operationId <type>", "add operation id").parse(process.argv);
-
-printExamples(program.operationId);
