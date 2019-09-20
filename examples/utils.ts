9ca195081409c7f188c660c16a3799372f37c077
reeli <reeli.null@gmail.com>
2019-09-20 11:20:33 +0800

    remove printer, cause you can implement it by yourself

--- examples/utils.ts
+++ examples/utils.ts
@@ -4,6 +4,10 @@ import traverse from "@babel/traverse";
 import * as t from "@babel/types";
 import { transformFromAstSync } from "@babel/core";
 import pathToRegexp from "path-to-regexp";
+import { Spec } from "swagger-schema-official";
+import { Traverse } from "../src/traverse";
+import * as fs from "fs";
+import { toFaker } from "../src/faker";
 
 export const prettifyCode = (code: string) =>
   prettier.format(code, {
@@ -51,3 +55,24 @@ export const isMatch = (routePattern: string) => (routePath: string) => {
   const regexp = pathToRegexp(routePattern);
   return !!regexp.exec(routePath);
 };
+
+export function printFaker(spec: Spec, definitionName?: string, outputFolderName = ".output") {
+  if (spec.definitions) {
+    const data = Traverse.of(spec.definitions).traverse();
+
+    if (!fs.existsSync(outputFolderName)) {
+      fs.mkdirSync(outputFolderName);
+    }
+
+    const faker = toFaker(data);
+    if (definitionName) {
+      fs.writeFileSync(
+        `${outputFolderName}/${definitionName}.json`,
+        JSON.stringify(faker[definitionName], null, 2),
+        "utf-8",
+      );
+    } else {
+      fs.writeFileSync(`${outputFolderName}/${spec.basePath}.json`, JSON.stringify(faker, null, 2), "utf-8");
+    }
+  }
+}
