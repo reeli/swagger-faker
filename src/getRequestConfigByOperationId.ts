5f30dc86ae598632225d4e79b68e50a3e9ed4c2a
reeli <reeli.null@gmail.com>
2019-09-24 10:47:15 +0800

    handle response

--- src/getRequestConfigByOperationId.ts
+++ src/getRequestConfigByOperationId.ts
@@ -1,4 +1,4 @@
-import {Operation, Spec} from "swagger-schema-official";
+import { Header, Operation, ParameterType, Reference, Response, Schema, Spec } from "swagger-schema-official";
 import { find, forEach, get, mapKeys, pick } from "lodash";
 import { pickRefKey } from "./utils";
 import { PathResolver } from "@ts-tool/ts-codegen";
@@ -8,9 +8,83 @@ const getPath = (pathName: string, basePath?: string) => {
   return pathName ? `${basePath}${subPath}` : subPath;
 };
 
-const getResponse = (operation: Operation) => {
-  const refKey = get(operation, "responses.200.schema.$ref") || get(operation, "responses.201.schema.$ref");
-  return pickRefKey(refKey);
+export interface IResponse {
+  schema: {
+    refKey?: string;
+    type?: ParameterType;
+    additionalProperties?: Schema;
+    properties?: Schema;
+  };
+  headers?: { [headerName: string]: Header };
+  examples?: { [exampleName: string]: {} };
+}
+
+const getResponseSchemaByName = (
+  responses: { [responseName: string]: Response | Reference },
+  responseName: string,
+): IResponse["schema"] => {
+  const response = get(responses, responseName);
+  if (!response) {
+    return {} as IResponse["schema"];
+  }
+
+  const reference = get(response, ["$ref"]);
+  if (reference) {
+    return {
+      refKey: pickRefKey(reference),
+    };
+  }
+
+  const schemaRef = get(response, ["schema", "$ref"]);
+
+  if (schemaRef) {
+    return {
+      refKey: pickRefKey(schemaRef),
+    };
+  }
+
+  const type = get(response, ["schema", "type"]);
+
+  if (type === "array") {
+    //TODO: 处理 array 中又嵌套 array 的 case
+    return {
+      refKey: pickRefKey(get(response, ["schema", "items", "$ref"])),
+      type: "array",
+    };
+  }
+
+  if (type === "object") {
+    // TODO: 处理 properties 中包含 $ref 的场景
+    const properties = get(response, ["schema", "properties"]);
+    const additionalProperties = get(response, ["schema", "additionalProperties"]);
+    return {
+      type: "object",
+      properties,
+      additionalProperties,
+    };
+  }
+
+  return {
+    type,
+  };
+};
+
+const getResponse = (operation: Operation): IResponse => {
+  // TODO: responses.200.schema.type ==="array"
+  // TODO: response.200.schema.type ==="object" (additionalProperties)
+  // TODO: response.200.schema.type==="string" | "number" | "integer" | "boolean"
+  // TODO: responses.200.headers 存在时
+  // TODO: responses.201 同上
+
+  const responses = get(operation, "responses");
+  const headers = get(operation, "responses.200.headers") || get(operation, "responses.201.headers");
+  const examples = get(operation, "responses.200.examples") || get(operation, "responses.201.examples");
+
+  return {
+    schema: getResponseSchemaByName(responses, "200") || getResponseSchemaByName(responses, "200"),
+    headers,
+    examples,
+  };
 };
 
 const getResolvedPathByOperationId = (swagger: Spec, operationId: string) => {
@@ -23,7 +97,7 @@ type TMethod = "get" | "post" | "put" | "delete" | "patch" | "options" | "head";
 export interface IRequestConfig {
   path: string;
   method: TMethod;
-  response: string;
+  response: IResponse;
   queryParams: string[];
   basePath: string;
 }
