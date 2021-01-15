3ba26bdbf1b1df255b5a0e705a48d1ccaa96f704
reeli <reeli.null@gmail.com>
2021-01-15 15:28:03 +0800

    feat

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,60 +1,90 @@
-import { getUnifiedInputs, CustomSpec, CustomReference, getPathsFromRef } from "@ts-tool/ts-codegen-core";
-import { keys, Dictionary, first, values } from "lodash";
+import { getUnifiedInputs, getPathsFromRef } from "@ts-tool/ts-codegen-core";
+import { keys, Dictionary, first, values, mapValues, upperCase } from "lodash";
 import fs from "fs";
-import { Spec } from "swagger-schema-official";
 import * as path from "path";
+import { IOpenAPI, IResponse, IComponents, IReference } from "./__types__/OpenAPI";
 
-export const bootstrap = (spec: CustomSpec) => {
-  const { basePath, paths } = getUnifiedInputs(spec);
-  keys(paths).forEach((path) => {
-    keys(paths[path]).forEach((method) => {
-      const res = {} as any;
-      res[paths[path][method].operationId] = {
-        path: `${basePath}/${path}`,
-        method,
-        responseData: getResponseData(paths[path][method].responses),
-      };
+interface Mock {
+  status: string;
+  response: any;
+  test: ({ query, params }: { query: object; params: object }) => boolean;
+}
+
+export interface Output {
+  operationId: string;
+  path: string;
+  method: string;
+  mocks: Mock[];
+  summary?: string;
+}
+
+export const bootstrap = (openapi: IOpenAPI) => {
+  const { basePath, paths } = getUnifiedInputs(openapi);
+  // TODO: remove any type later
+  const outputs: any[] = [];
+
+  mapValues(paths, (pathItem, pathName) => {
+    mapValues(pathItem, (schema, method) => {
+      outputs.push({
+        operationId: schema.operationId,
+        path: `${basePath}${pathName}`,
+        method: upperCase(method),
+        summary: schema.summary,
+        mocks: generateMocks(schema.responses, openapi),
+      });
     });
   });
+
+  return outputs;
 };
 
-export const getResponseData = <TResponse>(responses?: { [responseName: string]: TResponse | CustomReference }) => {
+const generateMocks = (responses: IComponents["responses"], spec: IOpenAPI) => {
+  return [getResponseData(responses, spec)];
+};
+
+export const getResponseData = (responses: IComponents["responses"], spec: IOpenAPI) => {
   if (!responses) {
     return;
   }
 
-  let firstSuccessResp: TResponse | CustomReference | null = null;
+  let successResp: { status: number; resp?: IReference | IResponse } | null = null;
 
   keys(responses).forEach((code) => {
-    const httpCode = Number(code);
-    const resp = responses[code];
-    const a = ((resp as any) || {}).schema;
-    const b = getFirstValue(((resp as any) || {}).content).schema;
-    const hasContent = getRef(resp) || a || b;
+    const resp = responses[code] as any;
+    const hasContent = getRef(resp) || getFirstValue(resp.content).schema;
 
-    if (httpCode >= 200 && httpCode < 300 && hasContent && !firstSuccessResp) {
-      firstSuccessResp = resp;
+    const httpCode = Number(code);
+    if (httpCode >= 200 && httpCode < 300 && hasContent && !successResp) {
+      successResp = {
+        status: httpCode,
+        resp,
+      };
     }
   });
 
-  return firstSuccessResp ? handleResponse<TResponse>(firstSuccessResp) : null;
+  return successResp
+    ? {
+        status: (successResp as any).status,
+        // TODO: remove any later
+        response: handleResponse(spec, (successResp as any).resp),
+      }
+    : null;
 };
 
-const handleResponse = <TResponse>(resp: TResponse | CustomReference) => {
+const handleResponse = (_: IOpenAPI, resp?: IReference | IResponse) => {
   if (getRef(resp)) {
     getPathsFromRef(resp.$ref);
     return;
   }
 
-  console.log(resp, "resp");
-  return;
+  return {};
 };
 
-const getRef = (v: any): v is CustomReference => v.$ref;
+const getRef = (v: any): v is IReference => v.$ref;
 
 const getFirstValue = (data?: Dictionary<any>) => first(values(data)) || {};
 
-const schemaStr = fs.readFileSync(path.resolve(__dirname, "../examples/swagger1.json"), "utf8");
+const schemaStr = fs.readFileSync(path.resolve(__dirname, "../examples/openapi.json"), "utf8");
 const finalSchemaStr = schemaStr.replace(/#\/definitions\/File/gi, "");
-const swagger = JSON.parse(finalSchemaStr) as Spec;
-bootstrap(swagger);
+const swagger = JSON.parse(finalSchemaStr) as IOpenAPI;
+console.log(JSON.stringify(bootstrap(swagger), null, 2));
