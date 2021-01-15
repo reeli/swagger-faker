32e2c09c3414c0057aaf03f6743e9c6a9e76486b
reeli <reeli.null@gmail.com>
2021-01-15 18:33:09 +0800

    feat: handle $ref in response

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,83 +1,86 @@
-import { getUnifiedInputs, getPathsFromRef } from "@ts-tool/ts-codegen-core";
-import { keys, Dictionary, first, values, mapValues, upperCase } from "lodash";
+import {getUnifiedInputs, getPathsFromRef} from "@ts-tool/ts-codegen-core";
+import {keys, Dictionary, first, values, mapValues, upperCase} from "lodash";
 import fs from "fs";
 import * as path from "path";
-import { IOpenAPI, IResponse, IComponents, IReference } from "./__types__/OpenAPI";
+import {IOpenAPI, IResponse, IComponents, IReference} from "./__types__/OpenAPI";
+import {putBackRefs} from "./putBackAllRefs";
 
 interface Mock {
-  status: string;
-  response: any;
-  test: ({ query, params }: { query: object; params: object }) => boolean;
+    status: string;
+    response: any;
+    test: ({query, params}: { query: object; params: object }) => boolean;
 }
 
 export interface Output {
-  operationId: string;
-  path: string;
-  method: string;
-  mocks: Mock[];
-  summary?: string;
+    operationId: string;
+    path: string;
+    method: string;
+    mocks: Mock[];
+    summary?: string;
 }
 
+const store = {};
+
 export const bootstrap = (openapi: IOpenAPI) => {
-  const { basePath, paths } = getUnifiedInputs(openapi);
-  // TODO: remove any type later
-  const outputs: any[] = [];
+    const {basePath, paths} = getUnifiedInputs(openapi);
+    // TODO: remove any type later
+    const outputs: any[] = [];
 
-  mapValues(paths, (pathItem, pathName) => {
-    mapValues(pathItem, (schema, method) => {
-      outputs.push({
-        operationId: schema.operationId,
-        path: `${basePath}${pathName}`,
-        method: upperCase(method),
-        summary: schema.summary,
-        mocks: generateMocks(schema.responses, openapi),
-      });
+    mapValues(paths, (pathItem, pathName) => {
+        mapValues(pathItem, (schema, method) => {
+            outputs.push({
+                operationId: schema.operationId,
+                path: `${basePath}${pathName}`,
+                method: upperCase(method),
+                summary: schema.summary,
+                mocks: generateMocks(schema.responses, openapi),
+            });
+        });
     });
-  });
 
-  return outputs;
+    return outputs;
 };
 
 const generateMocks = (responses: IComponents["responses"], spec: IOpenAPI) => {
-  return [getResponseData(responses, spec)];
+    return [getResponseData(responses, spec)];
 };
 
 export const getResponseData = (responses: IComponents["responses"], spec: IOpenAPI) => {
-  if (!responses) {
-    return;
-  }
-
-  let successResp: { status: number; resp?: IReference | IResponse } | null = null;
-
-  keys(responses).forEach((code) => {
-    const resp = responses[code] as any;
-    const hasContent = getRef(resp) || getFirstValue(resp.content).schema;
-
-    const httpCode = Number(code);
-    if (httpCode >= 200 && httpCode < 300 && hasContent && !successResp) {
-      successResp = {
-        status: httpCode,
-        resp,
-      };
+    if (!responses) {
+        return;
     }
-  });
 
-  return successResp
-    ? {
-        status: (successResp as any).status,
-        // TODO: remove any later
-        response: handleResponse(spec, (successResp as any).resp),
-      }
-    : null;
+    let successResp: { status: number; resp?: IReference | IResponse } | null = null;
+
+    keys(responses).forEach((code) => {
+        const resp = responses[code] as any;
+        const hasContent = getRef(resp) || getFirstValue(resp.content).schema;
+
+        const httpCode = Number(code);
+        if (httpCode >= 200 && httpCode < 300 && hasContent && !successResp) {
+            successResp = {
+                status: httpCode,
+                resp,
+            };
+        }
+    });
+
+    return successResp
+        ? {
+            status: (successResp as any).status,
+            // TODO: remove any later
+            response: handleResponse(spec, (successResp as any).resp),
+        }
+        : null;
 };
 
-const handleResponse = (_: IOpenAPI, resp?: IReference | IResponse) => {
-  if (getRef(resp)) {
-    getPathsFromRef(resp.$ref);
-    return;
-  }
+const handleResponse = (spec: IOpenAPI, resp?: IReference | IResponse) => {
+    if (getRef(resp)) {
+        getPathsFromRef(resp.$ref);
+        return;
+    }
 
-  return {};
+    return putBackRefs(store, spec.components?.schemas ?? {}, getFirstValue(resp?.content).schema);
 };
 
 const getRef = (v: any): v is IReference => v.$ref;
@@ -87,4 +90,4 @@ const getFirstValue = (data?: Dictionary<any>) => first(values(data)) || {};
 const schemaStr = fs.readFileSync(path.resolve(__dirname, "../examples/openapi.json"), "utf8");
 const finalSchemaStr = schemaStr.replace(/#\/definitions\/File/gi, "");
 const swagger = JSON.parse(finalSchemaStr) as IOpenAPI;
-console.log(JSON.stringify(bootstrap(swagger), null, 2));
+console.log(JSON.stringify(bootstrap(swagger), null, 2), '--------')
--- src/putBackAllRefs.ts
+++ src/putBackAllRefs.ts
@@ -1,27 +1,55 @@
 import { pickRefKey } from "./utils";
 import { mapValues, isArray, map } from "lodash";
-import { CustomSchema, CustomReference } from "@ts-tool/ts-codegen-core";
+import { ISchema, IReference } from "./__types__/OpenAPI";
 
-export const putBackAllRefs = (schemas: { [key: string]: CustomSchema | CustomReference }) => {
+export const putBackAllRefs = (schemas: { [key: string]: ISchema | IReference }) => {
   const store: { [key: string]: any } = {};
 
-  function pickRefData(s: CustomSchema) {
+  mapValues(schemas, (s, k) => {
+    if (store[k]) {
+      return store[k];
+    }
+
+    const v = putBackRefs(store, schemas, s);
+    store[k] = v;
+    return v;
+  });
+
+  return store;
+};
+
+export const putBackRefs = (
+  store: any,
+  schemas: { [key: string]: ISchema | IReference },
+  resp?: ISchema | IReference,
+) => {
+  if (!resp) {
+    return;
+  }
+
+  function pickRefData(s: ISchema | IReference) {
     const refKey = pickRefKey(s.$ref);
     if (store[refKey]) {
       return store[refKey];
     }
 
-    const resolvedRef = putBackRefs(schemas[refKey]);
+    const resolvedRef = putBack(schemas[refKey]);
     store[refKey] = resolvedRef;
     return resolvedRef;
   }
 
-  const putBackRefs = (schema: CustomSchema = {}): any => {
+  const putBack = (item: ISchema | IReference = {}): any => {
+    // handle ref
+    if (item.$ref) {
+      return pickRefData(item);
+    }
+
+    const schema = item as ISchema;
     // handle object
     if (schema.type === "object" || schema.properties) {
       return {
         ...schema,
-        properties: mapValues(schema.properties, (v3) => putBackRefs(v3)),
+        properties: mapValues(schema.properties, (v3) => putBack(v3)),
       };
     }
 
@@ -30,33 +58,28 @@ export const putBackAllRefs = (schemas: { [key: string]: CustomSchema | CustomRe
       if (isArray(schema.items)) {
         return {
           ...schema,
-          items: map(schema.items, (v) => putBackRefs(v)),
+          items: map(schema.items, (v) => putBack(v)),
         };
       }
       return {
         ...schema,
-        items: putBackRefs(schema.items),
+        items: putBack(schema.items),
       };
-    }
-
-    // handle ref
-    if (schema.$ref) {
-      return pickRefData(schema);
     }
 
     // handle oneOf
     if ((schema as any).oneOf) {
-      return map((schema as any).oneOf, (v) => putBackRefs(v));
+      return map((schema as any).oneOf, (v) => putBack(v));
     }
 
     // handle allOf
     if (schema.allOf) {
-      return map((schema as any).allOf, (v) => putBackRefs(v)).reduce((res, item) => {
+      return map((schema as any).allOf, (v) => putBack(v)).reduce((res, item1) => {
         return {
           ...res,
           properties: {
             ...res.properties,
-            ...item.properties,
+            ...item1.properties,
           },
         };
       }, {});
@@ -65,15 +88,5 @@ export const putBackAllRefs = (schemas: { [key: string]: CustomSchema | CustomRe
     return schema;
   };
 
-  mapValues(schemas, (s, k) => {
-    if (store[k]) {
-      return store[k];
-    }
-
-    const v = putBackRefs(s);
-    store[k] = v;
-    return v;
-  });
-
-  return store;
+  return putBack(resp);
 };
