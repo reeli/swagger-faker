58ee80bc745e90cde5ae8c9238a5fd76a35230c1
reeli <reeli.null@gmail.com>
2021-07-10 16:21:36 +0800

    add tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,19 @@
 import {fakerGenFromObj} from "../index";
 import openApi from "./mocks/openapi.json";
+import swagger from "./mocks/swagger.json";
 import {IOpenAPI} from "../__types__/OpenAPI";
-import {fakerGenFromObjExpectation1} from "./mocks/expectations";
+import {
+  fakerGenFromObjExpectationFromSwagger,
+  fakerGenFromObjExpectationFromOpenApi,
+} from "./mocks/expectations";
+import {Spec} from "swagger-schema-official";
 
-describe('#fakerGenFromObj', () => {
-  it('should generate fake date when given openapi object', () => {
-    expect(fakerGenFromObj(openApi as IOpenAPI, true)).toEqual(fakerGenFromObjExpectation1)
+describe("#fakerGenFromObj", () => {
+  it("should generate fake date when given openapi object", async () => {
+    expect(await fakerGenFromObj(openApi as IOpenAPI, true)).toEqual(fakerGenFromObjExpectationFromOpenApi);
+  });
+
+  it("should generate fake date when given swagger object", async () => {
+    expect(await fakerGenFromObj(swagger as Spec, true)).toEqual(fakerGenFromObjExpectationFromSwagger);
   });
 });
