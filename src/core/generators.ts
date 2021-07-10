9861b652381396abcaf182698f4c71478c759f82
reeli <reeli.null@gmail.com>
2021-07-10 14:12:00 +0800

    refactor code

--- src/core/generators.ts
+++ src/core/generators.ts
@@ -1,32 +1,49 @@
 import * as faker from "faker";
-import { toFakeData } from "./faker";
-import { SchemaWithoutRef } from "../__types__/common";
-import { mapValues, isArray } from "lodash";
+import {toFakeData} from "./toFakeData";
+import {SchemaWithoutRef} from "common";
+import {mapValues, isArray} from "lodash";
 
-export const booleanGenerator = () => faker.random.boolean();
-export const stringGenerator = (enums?: any[]) => {
-  const getRandomArrayItem = (items: any[]) => items[Math.floor(Math.random() * items.length)];
-  return enums ? getRandomArrayItem(enums) : faker.random.words();
-};
-export const numberGenerator = (max?: number, min?: number) =>
-  faker.random.number({
-    min,
-    max,
-  });
-export const fileGenerator = () => faker.system.mimeType();
-export const dateTimeGenerator = () => faker.date.past().toISOString();
-export const dateGenerator = () => dateTimeGenerator().slice(0, 10);
-export const timeGenerator = () => dateTimeGenerator().slice(11);
-export const urlGenerator = () => faker.internet.url();
-export const ipv4Generator = () => faker.internet.ip();
-export const ipv6Generator = () => faker.internet.ipv6();
-export const emailGenerator = () => faker.internet.email();
-
-export const objectGenerator = (schema: SchemaWithoutRef) => mapValues(schema.properties, (item) => toFakeData(item));
-export const arrayGenerator = (schema: SchemaWithoutRef): ReturnType<any> => {
-  if (isArray(schema.items)) {
-    return schema.items.map((item) => objectGenerator(item));
+export class FakeDataGenerator {
+  static of() {
+    return new FakeDataGenerator();
   }
 
-  return [toFakeData(schema.items!)];
-};
+  boolean = () => faker.random.boolean();
+
+  string = (enums?: any[]) => {
+    const getRandomArrayItem = (items: any[]) => items[Math.floor(Math.random() * items.length)];
+    return enums ? getRandomArrayItem(enums) : faker.random.words();
+  }
+
+  number = (max?: number, min?: number) =>
+    faker.random.number({
+      min,
+      max,
+    });
+
+  file = () => faker.system.mimeType();
+
+  dateTime = () => faker.date.past().toISOString();
+
+  date = () => this.dateTime().slice(0, 10);
+
+  time = () => this.dateTime().slice(11)
+
+  url = () => faker.internet.url();
+
+  ipv4 = () => faker.internet.ip();
+
+  ipv6 = () => faker.internet.ipv6();
+
+  email = () => faker.internet.email();
+
+  object = (schema: SchemaWithoutRef) => mapValues(schema.properties, (item) => toFakeData(item));
+
+  array = (schema: SchemaWithoutRef): ReturnType<any> => {
+    if (isArray(schema.items)) {
+      return schema.items.map((item) => this.object(item));
+    }
+
+    return [toFakeData(schema.items!)];
+  };
+}
