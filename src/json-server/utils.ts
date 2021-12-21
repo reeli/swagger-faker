417fc083597ce5f73dc25b5b0757d47f71e00421
reeli <reeli.null@gmail.com>
2021-12-21 21:17:43 +0800

    update config file

--- src/json-server/utils.ts
+++ src/json-server/utils.ts
@@ -1,8 +1,4 @@
 import prettier from "prettier";
-import { parse } from "@babel/parser";
-import traverse from "@babel/traverse";
-import * as t from "@babel/types";
-import { transformFromAstSync } from "@babel/core";
 import { pathToRegexp } from "path-to-regexp";
 
 export const prettifyCode = (code: string) =>
@@ -12,41 +8,6 @@ export const prettifyCode = (code: string) =>
     arrowParens: "always",
     parser: "babel",
   });
-
-export const insertStrToDB = (fileStr: string, propName: string, mockDataFolderPath: string) => {
-  let beforeReturn: any;
-  let isExists: boolean = false;
-
-  const ast = parse(fileStr, { sourceType: "module" });
-  traverse(ast, {
-    ReturnStatement: (path) => {
-      beforeReturn = path;
-    },
-    VariableDeclaration: (path) => {
-      path.node.declarations.map((node) => {
-        if ((node.id as any).name === propName) {
-          isExists = true;
-        }
-      });
-    },
-    ObjectExpression: ({ node }) => {
-      node.properties.push(t.objectProperty(t.identifier(propName), t.identifier(propName), undefined, true));
-    },
-  });
-
-  if (isExists) {
-    console.log(`${propName} already exists in your mock server file!`);
-    return;
-  }
-
-  if (beforeReturn) {
-    beforeReturn.insertBefore(
-      parse(`const ${propName} = require("${mockDataFolderPath}/${propName}.json");`, { sourceType: "module" }),
-    );
-  }
-
-  return transformFromAstSync(ast, undefined, { compact: true });
-};
 
 export const isMatch = (routePattern: string) => (routePath: string) => {
   const regexp = pathToRegexp(routePattern);
